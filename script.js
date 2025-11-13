f5f4f92796a563a7484511b33e1f5ca25e84d525
ivanchikb360 <ivanchikb360@gmail.com>
2025-11-13 11:01:49 -0800

    legal

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 // Sample quotes data
 const quotes = [
     { name: 'Mary Scott', make: 'Mazda', state: 'WY', rate: '$60/mo' },
-    { name: 'Susan Flores', make: 'Nissan', state: 'KY', rate: '$44/mo' },
+    { name: 'Susan Flores', make: 'Nissan', state: 'KY', rate: '$64/mo' },
     { name: 'Amanda Thompson', make: 'Subaru', state: 'DE', rate: '$69/mo' },
-    { name: 'Sarah Miller', make: 'Hyundai', state: 'SC', rate: '$53/mo' },
-    { name: 'Thomas White', make: 'Jeep', state: 'OR', rate: '$34/mo' },
+    { name: 'Sarah Miller', make: 'Hyundai', state: 'SC', rate: '$63/mo' },
+    { name: 'Thomas White', make: 'Jeep', state: 'OR', rate: '$64/mo' },
     { name: 'Kimberly King', make: 'Honda', state: 'NE', rate: '$77/mo' },
     { name: 'Matthew Williams', make: 'Kia', state: 'NV', rate: '$77/mo' },
-    { name: 'Heather Johnson', make: 'Mazda', state: 'WI', rate: '$41/mo' },
+    { name: 'Heather Johnson', make: 'Mazda', state: 'WI', rate: '$61/mo' },
     { name: 'Angela Martinez', make: 'Jeep', state: 'PA', rate: '$77/mo' },
-    { name: 'Emily Hill', make: 'Nissan', state: 'ND', rate: '$28/mo' }
+    { name: 'Emily Hill', make: 'Nissan', state: 'ND', rate: '$62/mo' }
 ];
 
 // Additional quotes for rotation
 const additionalQuotes = [
-    { name: 'Robert Davis', make: 'Ford', state: 'TX', rate: '$52/mo' },
+    { name: 'Robert Davis', make: 'Ford', state: 'TX', rate: '$62/mo' },
     { name: 'Jennifer Brown', make: 'Toyota', state: 'CA', rate: '$65/mo' },
-    { name: 'David Wilson', make: 'Chevrolet', state: 'FL', rate: '$48/mo' },
+    { name: 'David Wilson', make: 'Chevrolet', state: 'FL', rate: '$68/mo' },
     { name: 'Lisa Anderson', make: 'BMW', state: 'NY', rate: '$89/mo' },
     { name: 'Michael Taylor', make: 'Mercedes', state: 'IL', rate: '$95/mo' },
     { name: 'Karen Thomas', make: 'Audi', state: 'WA', rate: '$82/mo' }
